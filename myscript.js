74048b7fbc27467cda62594872c5c0c087ce4278
Kirsten Korevaar <kirsten@eml.cc>
2015-06-08 15:13:32 -0400

    Add placeholder animation

--- myscript.js
+++ myscript.js
@@ -1,12 +1,12 @@
 function handleclick(){
 	// Short animation spinning the colors
-	//setTimeout(display("test1"), 5000)
-	//console.log("purple")
-    coin_toss()
-	window.setTimeout(reload_page, 1500);
+    display("test");
+    text("");
+    window.setTimeout(coin_toss, 2000);	
 }
 
 function coin_toss(){
+
 	if (Math.random() > 0.5){
 		display("yes");
 		text("yes");
@@ -15,6 +15,7 @@ function coin_toss(){
 		display("no");
 		text("no");
 	}
+	window.setTimeout(reload_page, 1500);
 }
 
 function reload_page(){
