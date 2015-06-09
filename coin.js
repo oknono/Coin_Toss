2c39bc70781c4680a8f6d0fb885d6d1727befcf9
Kirsten Korevaar <kirsten@eml.cc>
2015-06-09 12:07:23 -0400

    Update color in CSS

---
+++ coin.js
@@ -0,0 +1,35 @@
+function handleclick(){
+	// Short animation spinning the colors
+    display("test");
+    window.setTimeout(coin_toss, 500);	
+}
+
+function coin_toss(){
+    // shows outcome of coin toss and reloads page
+	if (Math.random() > 0.5){
+		display("yes");
+	}
+	else {
+		display("no");
+	}
+	window.setTimeout(reload_page, 1500);
+}
+
+function reload_page(){
+	location.reload();
+}
+
+function display(outcome){
+	//change_headings = document.getElementById("visible");
+    //change_headings.setAttribute("visible", "invisible");
+	change_class = document.getElementById("root");
+    change_class.setAttribute("class", outcome);
+    change_text = document.getElementById("p_text");
+    if (outcome == "test"){
+        change_text.innerHTML = "?";
+    }
+    else{
+        change_text.innerHTML = outcome.toUpperCase();
+    }
+}
+
