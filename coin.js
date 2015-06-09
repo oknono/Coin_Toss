bd581c8f5e6d82339825d2277997a92a17fb256f
Kirsten Korevaar <kirsten@eml.cc>
2015-06-09 12:35:29 -0400

    Add h1 and h2

--- coin.js
+++ coin.js
@@ -20,8 +20,6 @@ function reload_page(){
 }
 
 function display(outcome){
-	//change_headings = document.getElementById("visible");
-    //change_headings.setAttribute("visible", "invisible");
 	change_class = document.getElementById("root");
     change_class.setAttribute("class", outcome);
     change_text = document.getElementById("p_text");
@@ -31,5 +29,6 @@ function display(outcome){
     else{
         change_text.innerHTML = outcome.toUpperCase();
     }
+    
 }
 
